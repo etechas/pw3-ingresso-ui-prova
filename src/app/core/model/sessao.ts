284b593870eed7722936981f9a6657e0a9bdbf16
Chvxs01 <gchaves961@gmail.com>
2026-06-23 08:48:33 -0300

    Atividade finalizada!

--- src/app/core/model/sessao.ts
+++ src/app/core/model/sessao.ts
@@ -1,5 +1,11 @@
+import { Filme } from "./filme";
 
 export interface Sessao {
   id: number;
-  
+  dia?: Date;
+  horario: Date;
+  preco?: number;
+  filme: Filme[];
+  sala?: string;
+
 }
--- src/app/core/services/filme-service.ts
+++ src/app/core/services/filme-service.ts
@@ -2,10 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Filme } from '../model/filme';
+import { Sessao } from '../model/sessao';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class FilmeService {
   private apiUrl = 'http://localhost:8090/filmes';
   private http = inject(HttpClient);
@@ -13,13 +15,17 @@ export class FilmeService {
   findCartaz(): Observable<Filme[]>{
     return this.http.get<Filme[]>(`${this.apiUrl}/em-cartaz`);
   }
-  
-  //findById(id: Number): Observable<Filme>{
-  //  return null;
-  //}
 
-  //findSessoesByFilmeId(id: Number): Observable<Sessao[]>{
+  findAll(): Observable<Filme[]>{
+    return this.http.get<Filme[]>(`${this.apiUrl}`)
+  }
+  
+  // findById(id: Number): Observable<Filme>{
   //  return null;
-  //}
+  // }
+
+  // findSessoesByFilmeId(id: Number): Observable<Sessao[]>{
+  //  return null;
+  // }
 
 }
